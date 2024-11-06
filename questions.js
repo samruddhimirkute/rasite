1a36e0525b748897a51fb13f2d140b9de6e7d7e8
samruddhimirkute <samruddhimirkute@gmail.com>
2024-11-06 22:01:45 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What are the three phases of progression in rheumatoid arthritis (RA)?",
+    answer: "B) Initiation Phase, Amplification Phase, Chronic Inflammatory Phase",
+    options: [
+      "A) Initiation Phase, Inflammatory Phase, Recovery Phase",
+      "B) Initiation Phase, Amplification Phase, Chronic Inflammatory Phase",
+      "C) Acute Phase, Chronic Phase, Remission Phase",
+      "D) Early Phase, Middle Phase, Late Phase"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following can be a complication of rheumatoid arthritis affecting the spine?",
+    answer: "B) Myelopathy due to atlanto-axial subluxation",
+    options: [
+      "A) Atherosclerosis",
+      "B) Myelopathy due to atlanto-axial subluxation",
+      "C) Osteoporosis in the legs",
+      "D) Periodontitis in the gums"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following systemic effects can RA have on the body?",
+    answer: "B) Inflammation around the lungs and heart",
+    options: [
+      "A) Increased bone density",
+      "B) Inflammation around the lungs and heart",
+      "C) Improved joint flexibility",
+      "D) Enhanced red blood cell production"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is NOT a common symptom or effect associated with rheumatoid arthritis (RA)?",
+    answer: "C) Development of a high red blood cell count",
+    options: [
+      "A) Chronic pain and stiffness in joints",
+      "B) Inflammation of the synovial membrane",
+      "C) Development of a high red blood cell count",
+      "D) Dry eyes and mouth"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What type of disease is rheumatoid arthritis (RA)?",
+    answer: "B) Chronic autoimmune disease",
+    options: [
+      "A) Bacterial infection",
+      "B) Chronic autoimmune disease",
+      "C) Acute viral infection",
+      "D) Genetic disorder"
+    ]
+  },
+
+]
